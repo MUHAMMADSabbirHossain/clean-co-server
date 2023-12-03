c97faf229bc2c264ad48184b6173ddf835b9cab0
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-03 19:14:31 +0600

    mongodb: connected successfully.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,35 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+
+const uri = "mongodb+srv://dustobalok000:WAmEARwXrcrUEkTY@clean-co.c3skspr.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const servicesCollection = client.db("cleanCo").collection("service");
+
+        app.get("/service", async (req, res) => {
+            const services = await servicesCollection.find({}).toArray();
+            console.log(services);
+            res.send(services);
+        });
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get("/", async (req, res) => {
     res.send(
